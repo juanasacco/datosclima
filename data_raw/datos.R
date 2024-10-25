c4b01b3b32c38faeca642debb78c41c6cbdd82d6
juanasacco <jua.sacco04@gmail.com>
2024-10-25 12:23:35 -0300

    modifique el nombre de los archivos para que sean más accesibles

--- data_raw/datos.R
+++ data_raw/datos.R
@@ -7,11 +7,11 @@ estacion_NH0437_link <- "https://raw.githubusercontent.com/rse-r/intro-programac
 metadatos_link <- "https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/metadatos_completos.csv"
 
 archivo_metadatos <- "data_raw/metadatos.csv"
-archivo_NH0472 <- "data_raw/estacion_NH0472.csv"
-archivo_NH0910 <- "data_raw/estacion_NH0910.csv"
-archivo_NH0046 <- "data_raw/estacion_NH0046.csv"
-archivo_NH0098 <- "data_raw/estacion_NH0098.csv"
-archivo_NH0437 <- "data_raw/estacion_NH0437.csv"
+archivo_NH0472 <- "data_raw/NH0472.csv"
+archivo_NH0910 <- "data_raw/NH0910.csv"
+archivo_NH0046 <- "data_raw/NH0046.csv"
+archivo_NH0098 <- "data_raw/NH0098.csv"
+archivo_NH0437 <- "data_raw/NH0437.csv"
 
 download.file(metadatos_link,archivo_metadatos)
 download.file(estacion_NH0472_link,archivo_NH0472)
@@ -21,12 +21,12 @@ download.file(estacion_NH0098_link,archivo_NH0098)
 download.file(estacion_NH0437_link,archivo_NH0437)
 
 metadatos <- readr::read_csv("data_raw/metadatos.csv")
-estacion_NH0472 <- readr::read_csv("data_raw/estacion_NH0472.csv")
-estacion_NH0910 <- readr::read_csv("data_raw/estacion_NH0910.csv")
-estacion_NH0046 <- readr::read_csv("data_raw/estacion_NH0046.csv")
-estacion_NH0098 <- readr::read_csv("data_raw/estacion_NH0098.csv")
-estacion_NH0437 <- readr::read_csv("data_raw/estacion_NH0437.csv")
+NH0472 <- readr::read_csv("data_raw/NH0472.csv")
+NH0910 <- readr::read_csv("data_raw/NH0910.csv")
+NH0046 <- readr::read_csv("data_raw/NH0046.csv")
+NH0098 <- readr::read_csv("data_raw/NH0098.csv")
+NH0437 <- readr::read_csv("data_raw/NH0437.csv")
 
-datos_estaciones <- rbind(estacion_NH0472, estacion_NH0910, estacion_NH0046, estacion_NH0098,estacion_NH0437)
+datos_estaciones <- rbind(NH0472, NH0910, NH0046,NH0098,NH0437)
 
 
