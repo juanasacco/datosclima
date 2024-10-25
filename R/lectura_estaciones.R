41185868f4ba8f5dd21aaa6b34366b174140e0ff
juanasacco <jua.sacco04@gmail.com>
2024-10-25 12:25:10 -0300

    modifique los nombres del id que estaban mal escritos

--- R/lectura_estaciones.R
+++ R/lectura_estaciones.R
@@ -1,13 +1,12 @@
 
 lectura_estaciones <- function(id_estacion, ruta) {
-  # Asignar la URL de la estación según el ID
   estacion_url <- switch(id_estacion,
                          "NH0046" = "https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/NH0046.csv",
                          "NH0098" = "https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/NH0098.csv",
-                         "NH00437" = "https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/NH0437.csv",
-                         "NH00910" = "https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/NH0910.csv",
-                         "NH00472" = "https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/NH0472.csv",
-                         stop("ID de estación no reconocido"))
+                         "NH0437" = "https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/NH0437.csv",
+                         "NH0910" = "https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/NH0910.csv",
+                         "NH0472" = "https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/NH0472.csv",
+                         stop("ID de estación no encontrado"))
 
   if (file.exists(ruta)) {
     id_estacion <- readr::read_csv(ruta)
