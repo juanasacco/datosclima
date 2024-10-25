ab33a6f69965bbc3b644d2bf0c8c62207e91be41
juanasacco <jua.sacco04@gmail.com>
2024-10-25 11:34:26 -0300

    modifique las funciones para que funcionen correctamente

--- R/grafico_temperatura_mensual.R
+++ R/grafico_temperatura_mensual.R
@@ -3,10 +3,11 @@ grafico_temperatura_mensual<- function(dataframe, color = colors(distinct = TRUE
   dataframe |>
     dplyr::mutate(fecha = as.Date(fecha)) |>
     dplyr::mutate(anio = year(fecha), mes = month(fecha)) |>
-    dplyr::group_by(id, mes) |>
-    dplyr::summarise(temperatura_abrigo = mean(temperatura_abrigo_150cm, na.rm = TRUE))
+    dplyr::group_by(id, mes) |>E))
 
   return(ggplot2::ggplot(aes(mes, temperatura_abrigo, color = color)) +
            geom_point() )
 
 }
+
+
--- R/lectura_estaciones.R
+++ R/lectura_estaciones.R
@@ -1,15 +1,23 @@
 
 lectura_estaciones <- function(id_estacion, ruta) {
-  estacion_url <-
+  # Asignar la URL de la estación según el ID
+  estacion_url <- switch(id_estacion,
+                         "NH0046" = "https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/NH0046.csv",
+                         "NH0098" = "https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/NH0098.csv",
+                         "NH00437" = "https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/NH0437.csv",
+                         "NH00910" = "https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/NH0910.csv",
+                         "NH00472" = "https://raw.githubusercontent.com/rse-r/intro-programacion/main/datos/NH0472.csv",
+                         stop("ID de estación no reconocido"))
 
-    if(file.exists(ruta)) {
-      id_estacion <- readr::read_csv(ruta)
-      cli::cli_inform("El archivo ya existia, se leyo el archivo")}
+  if (file.exists(ruta)) {
+    id_estacion <- readr::read_csv(ruta)
+    cli::cli_inform("El archivo ya existía, se leyó el archivo.")
+  }
 
   else {
     download.file(estacion_url, ruta)
     id_estacion <- readr::read_csv(ruta)
-    cli::cli_inform("El archivo no existia, por lo cual se descargo y luego se leyo")
+    cli::cli_inform("El archivo no existía, por lo cual se descargó y luego se leyó.")
   }
   return(id_estacion)
 }
