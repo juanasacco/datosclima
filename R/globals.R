441218d370783524c1e5b0655fee894f8fc5298b
juanasacco <jua.sacco04@gmail.com>
2024-11-15 21:12:09 -0300

    solucionamos la nota de las variables globales

---
+++ R/globals.R
@@ -0,0 +1,5 @@
+utils::globalVariables(c(
+  "fecha", "id", "mes", "temperatura_abrigo_150cm",
+  "temperatura_abrigo", "download.file",
+  "temperatura_abrigo_150cm_minima", "temperatura_intemperie_5cm_minima"
+))
